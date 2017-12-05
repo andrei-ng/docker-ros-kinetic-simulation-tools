9aa649684b888f0b31836b2d3a0941b4297ab469
Andrei Gherghescu <g.andrei.n@gmail.com>
2017-12-05 16:36:14 +0100

    Updated run.sh script to check nvidia-docker version

--- run.sh
+++ run.sh
@@ -10,14 +10,42 @@ set -e
 
 IMAGE_NAME=$1 && shift 1
 
-# Run the container with NVIDIA Graphics acceleration, shared network interface, shared X11
-nvidia-docker run --rm\
-  --net=host \
-  --ipc=host \
-  --privileged \
-  -e DISPLAY=$DISPLAY \
-  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
-  -v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
-  -e ROS_HOSTNAME=andrei-lp7510ub \
-  -it $IMAGE_NAME "$@"
+THIS_HOST=`hostname`
+NVIDIA_DOCKER_VERSION=$(dpkg -l | grep nvidia-docker | awk '{ print $3 }' | awk -F'[_.]' '{print $1}')
+
+# Run the container with NVIDIA Graphics acceleration, shared network interface, shared hostname, shared X11
+if [ $NVIDIA_DOCKER_VERSION = "2" ]; then
+  docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 --rm \
+    --net=host \
+    --ipc=host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
+    -v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
+    -e ROS_HOSTNAME=$THIS_HOST \
+    -it $IMAGE_NAME "$@"
+elif [ $NVIDIA_DOCKER_VERSION = "1" ]; then
+  nvidia-docker run --rm \
+    --net=host \
+    --ipc=host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
+	-v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
+    -e ROS_HOSTNAME=$THIS_HOST \
+    -it $IMAGE_NAME "$@"
+else
+  echo "[Warning] nvidia-docker not installed, running docker without Nvidia hardware acceleration / OpenGL support"
+  docker run --rm \
+    --net=host \
+    --ipc=host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
+    -v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
+    -e ROS_HOSTNAME=$THIS_HOST \
+    -it $IMAGE_NAME "$@"
+fi
