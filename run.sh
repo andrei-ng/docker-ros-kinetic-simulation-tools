3ab1903907f8ced87dc15dc89fd3dd78c22540db
Andrei Gherghescu <g.andrei.n@gmail.com>
2017-12-12 16:20:58 +0100

    Removed duplicate code in run script

--- run.sh
+++ run.sh
@@ -13,39 +13,33 @@ IMAGE_NAME=$1 && shift 1
 THIS_HOST=`hostname`
 NVIDIA_DOCKER_VERSION=$(dpkg -l | grep nvidia-docker | awk '{ print $3 }' | awk -F'[_.]' '{print $1}')
 
-# Run the container with NVIDIA Graphics acceleration, shared network interface, shared hostname, shared X11
-if [ $NVIDIA_DOCKER_VERSION = "2" ]; then
-  docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 --rm \
-    --net=host \
-    --ipc=host \
-    --privileged \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
-    -v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
-    -e ROS_HOSTNAME=$THIS_HOST \
-    -it $IMAGE_NAME "$@"
-elif [ $NVIDIA_DOCKER_VERSION = "1" ]; then
-  nvidia-docker run --rm \
-    --net=host \
-    --ipc=host \
-    --privileged \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
-	-v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
-    -e ROS_HOSTNAME=$THIS_HOST \
-    -it $IMAGE_NAME "$@"
+# Determine the appropriate version of the docker run command
+if [ $NVIDIA_DOCKER_VERSION = "1" ]; then
+    docker_run_cmd="nvidia-docker run --rm"
+elif [ $NVIDIA_DOCKER_VERSION = "2" ]; then
+  docker_run_cmd="docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 --rm"
 else
   echo "[Warning] nvidia-docker not installed, running docker without Nvidia hardware acceleration / OpenGL support"
-  docker run --rm \
+  docker_run_cmd="docker run --rm"
+fi
+
+# Deterimine configured user for the docker image
+docker_user=$(docker image inspect --format '{{.Config.User}}' $IMAGE_NAME)
+if [ "$docker_user" = "" ]; then
+    home_folder="/root"
+else
+    home_folder="/home/$docker_user"    
+fi
+
+# Run the container with NVIDIA Graphics acceleration, shared network interface, shared hostname, shared X11
+$(echo $docker_run_cmd) \
     --net=host \
     --ipc=host \
     --privileged \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $HOME/.Xauthority:/root/.Xauthority -e XAUTHORITY=/root/.Xauthority \
+    -v $HOME/.Xauthority:$home_folder/.Xauthority \
+    -e XAUTHORITY=$home_folder/.Xauthority \
     -v $HOME/Projects/devs/simulation/v-rep:/extern/v-rep \
     -e ROS_HOSTNAME=$THIS_HOST \
     -it $IMAGE_NAME "$@"
-fi
